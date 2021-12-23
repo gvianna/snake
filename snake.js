aba9e7123a11fb83c83454a2f0bd22a78e49857f
gvianna <gjosevianna@gmail.com>
2021-12-23 08:31:12 -0300

    Initial commit

---
+++ snake.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+  var stage = document.getElementById('stage');
+  var context = stage.getContext('2d');
+
+  context.fillStyle = 'black';
+  context.fillStyle(0, 0, stage.width, stage.height);
+}
